cd3b6fad813492c95feba6d7c02e889bee9a2ca3
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-29 22:10:33 +0300

    created models

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from . import forms 
+from django.views.generic import CreateView
+
 
 # Create your views here.
+class SignUp(CreateView)
