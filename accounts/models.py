0f75b3e77f9141f146bc389e6a6503ac67ad174b
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-30 11:28:32 +0300

    debugging errors

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib import auth
-
+from django.contrib.auth.models import User, PermissionsMixin
 
 # Create your models here.
-class User(auth.models.User, auth.models.PermissionsMixin):
+class User(auth.models.User, PermissionsMixin):
     def __str__(self):
-        return "@{}".format(self.username)
+        return f"@{username}"
